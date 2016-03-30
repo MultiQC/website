d303a227ff2a6c91b5ba3cd60cfd4913308e5a9d
Phil Ewels <phil.ewels@scilifelab.se>
2016-03-30 16:57:53 +0200

    Reworked the demo iframe. osx buttons now work, closes #6. Has tabs, closes #7

--- public_html/js/homepage.js
+++ public_html/js/homepage.js
@@ -10,5 +10,22 @@ $(function () {
      $('.install-switcher .btn').removeClass('active');
      $(this).addClass('active');
   });
-
+  
+  // iFrame demo window buttons
+  $('#iframe_browser_buttons span:first-of-type, #iframe_browser_buttons span:nth-of-type(2)').click(function(e){
+    $('#iframe_browser').slideUp();
+    $('#mqc_homepage .header').animate({'margin-bottom': 30})
+    $('#mqc_homepage').removeClass('hasDemoiFrame');
+  });
+  $('#iframe_browser_buttons span:last-of-type').click(function(e){
+    window.location = $('#iframe_browser iframe').attr('src');
+  });
+  
+  // iFrame demo tabs
+  $('#iframe_browser_tabs li a').click(function(e){
+    e.preventDefault();
+    $('#iframe_browser_tabs li').removeClass('active');
+    $(this).parent().addClass('active');
+    $('#iframe_browser iframe').attr('src', $(this).attr('href'));
+  });
 });
