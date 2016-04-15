f80a3f21f18611b8b768a4d0152f9d82708a1e91
Phil Ewels <phil.ewels@scilifelab.se>
2016-04-15 14:15:53 +0200

    Restructured demo videos and wrote example report switcher. Got everything working. Just needs some content now...

--- public_html/js/homepage.js
+++ public_html/js/homepage.js
@@ -11,6 +11,16 @@ $(function () {
      $(this).addClass('active');
   });
   
+  // Video switcher
+  $('.video-chooser a').click(function(e){
+    if($('#multiqc-video').is(':visible')){
+      e.preventDefault();
+      $('.video-chooser a').removeClass('active');
+      $(this).addClass('active');
+      $('#multiqc-video').attr('src', $(this).data('src'));
+    }
+  });
+  
   // iFrame demo window buttons
   $('#iframe_browser_buttons span:first-of-type, #iframe_browser_buttons span:nth-of-type(2)').click(function(e){
     $('#iframe_browser').slideUp();
@@ -19,11 +29,19 @@ $(function () {
     window.location = $('#iframe_browser iframe').attr('src');
   });
   
-  // iFrame demo tabs
-  $('#iframe_browser_tabs li a').click(function(e){
+  // Switch report links / iFrame tab click
+  $('.demo-chooser a, #iframe_browser_tabs li a').click(function(e){
     e.preventDefault();
-    $('#iframe_browser_tabs li').removeClass('active');
-    $(this).parent().addClass('active');
-    $('#iframe_browser iframe').attr('src', $(this).attr('href'));
+    var href = $(this).attr('href');
+    $('#iframe_browser_tabs li, .demo-chooser a').removeClass('active');
+    $('.demo-chooser a[href="'+href+'"]').addClass('active');
+    $('#iframe_browser_tabs li a[href="'+href+'"]').parent().addClass('active');
+    $('#iframe_browser iframe').attr('src', href);
+    var descrip_id = $('.demo-chooser a.active').data('target');
+    $('.demo-descriptions > div').hide();
+    $(descrip_id).show();
   });
+  
 });
+
+
