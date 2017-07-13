4e7350a934f27f90219d0884d593bb14fbbecc49
Phil Ewels <phil.ewels@scilifelab.se>
2017-07-13 14:13:21 +0200

    Made version check stats page work properly. Removed PyPI downloads. Added 404 page.

--- public_html/usage.php
+++ public_html/usage.php
@@ -6,37 +6,40 @@ $dbconfig = parse_ini_file("../config.ini");
 $db = new mysqli('localhost', $dbconfig['user'], $dbconfig['password'], $dbconfig['db']);
 if($db->connect_errno > 0){ die('Unable to connect to database [' . $db->connect_error . ']'); }
 
-// Get the download stats
-$dl_stats;
-$versions;
-$sql = "SELECT `version`, `date` FROM `version_check` ORDER BY `date` ASC";
-if(!$result = $db->query($sql)){ die('There was an error running the query [' . $db->error . ']'); }
-while($r = $result->fetch_assoc()){
-    $t = $r['date'];
-    $v = $r['version'];
-    if(!isset($versions[$v])){
-      $versions[$v] = 1;
-    } else {
-      $versions[$v] += 1;
+// Usage per week, by version
+if ($result = $db->query("SELECT `version`, COUNT(*) as `version_count`, `date` from `version_check` GROUP BY `version`, WEEK(`date`) ORDER BY `date` ASC, `version` ASC")) {
+    $versions_by_week = [];
+    $weeks = [];
+    while ($row = $result->fetch_assoc()) {
+      $v = str_replace('.dev', '', $row['version']);
+      if(!array_key_exists($v, $versions_by_week)){
+        $versions_by_week[$v] = array(
+          'x' => [],
+          'y' => [],
+          'name' => $v,
+          'type' => 'bar'
+        );
+      }
+      $monday = date("Y-m-d", strtotime('last monday', strtotime($row['date'])));
+      $versions_by_week[$v]['x'][] = $monday;
+      $versions_by_week[$v]['y'][] = $row['version_count'];
+      if(!in_array($monday, $weeks)){
+        $weeks[] = $monday;
+      }
     }
-    foreach (array_keys($versions) as $k){
-      $dl_stats[$t][$k] = $versions[$k];
-    }
+    $result->close();
+    // Sort the versions
+    ksort($versions_by_week);
+    // Remove dodgy looking spike of runs
+    $idx = array_search('2016-08-29', $versions_by_week['0.7']['x']);
+    unset($versions_by_week['0.7']['x'][$idx]);
+    unset($versions_by_week['0.7']['y'][$idx]);
+    $versions_by_week['0.7']['x'] = array_values($versions_by_week['0.7']['x']);
+    $versions_by_week['0.7']['y'] = array_values($versions_by_week['0.7']['y']);
+} else {
+  echo '<h1>SQL query failed!</h1>';
+  echo '<pre>'.$db->error.'</pre>';
 }
-
-// Create the JSON strings needed by HighCharts
-$categories = array_keys($dl_stats);
-$series = [];
-foreach(array_keys($versions) as $v){
-    $data = [];
-    foreach($categories as $t){
-        $t_dl = isset($dl_stats[$t][$v]) ? intval($dl_stats[$t][$v]) : NULL;
-        // Javascript uses milliseconds, so multiply timestamp by 1000
-		    array_push($data, array(strtotime($t)*1000, $t_dl));
-    }
-    array_push($series, array('name' => $v, 'data' => $data));
-}
-$series_json = json_encode($series);
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -44,8 +47,8 @@ $series_json = json_encode($series);
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <title>Usage: MultiQC</title>
+
+    <title>Version Check Stats: MultiQC</title>
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
@@ -69,7 +72,7 @@ $series_json = json_encode($series);
   <body id="mqc_docs">
 
     <div class="header">
-      
+
       <!-- Static navbar -->
       <nav class="navbar navbar-default navbar-inverse navbar-static-top">
         <div class="container">
@@ -84,20 +87,8 @@ $series_json = json_encode($series);
           </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="index.html">Home</a></li>
+              <li><a href="index.php">Home</a></li>
               <li><a href="docs/">Docs</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Example Reports <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li class="dropdown-header">Analysis Types</li>
-                  <li><a href="#">Whole Genome</a></li>
-                  <li><a href="examples/rna-seq/multiqc_report.html">RNA-Seq</a></li>
-                  <li><a href="examples/bs-seq/multiqc_report.html">Bisulfite</a></li>
-                  <li class="dropdown-header">Report Variants</li>
-                  <li><a href="#">Many Samples</a></li>
-                  <li><a href="#">Directory Names</a></li>
-                </ul>
-              </li>
               <li><a href="https://www.github.com/ewels/MultiQC">GitHub</a></li>
             </ul>
           </div><!--/.nav-collapse -->
@@ -113,27 +104,32 @@ $series_json = json_encode($series);
             </h1>
           </div>
           <div class="col-sm-6" style="margin-top:40px;">
-            <p class="lead">Usage Statistics</p>
+            <p class="lead">Version Check Statistics</p>
           </div>
         </div>
       </div>
     </div>
-			
+
     <div class="container"><div class="content_block">
-			<h2>Usage Stats</h2>
-			<p>Version check call frequencies.</p>
-			<div id="multiqc_version_checks_plot"></div>
+			<h2>MultiQC Version Call Stats</h2>
+      <p>When MultiQC runs, it checks <code>multiqc.info</code> for the latest released version so that it can issue a warning if the software is out of date.
+        The website records the date of each check, along with the version of MultiQC that was running. This allows us to roughly plot usage.</p>
+			<p>Note that numbers should be taken with a large pinch of suspicion. For example, the check from MultiQC in Python 2.7 call probably only works after the v1.1 release.
+      Also people can opt-out of the check and may be running offline.</p>
+      <p>A spike of over thirteen thousand v0.7 runs in one week (2016-08-29) has been removed to improve visualisation.</p>
+      <p>Development versions merged into main release numbers for plot. <em>eg.</em> <code>v1.1dev</code> shows as <code>1.1</code>.</p>
+      <div id="versions_by_week" style="height:450px;"></div>
     </div></div>
 
 
     </div> <!-- /container -->
-    
+
     <footer id="footer">
       <div class="container">
         <p>Created by Phil Ewels:
           <a target="_blank" href="https://github.com/ewels"><i class="fa fa-github"></i> ewels</a> |
           <a target="_blank" href="https://twitter.com/tallphil"><i class="fa fa-twitter"></i> tallphil</a> |
-          <a target="_blank" href="https://se.linkedin.com/in/philewels">LinkedIn</a> | 
+          <a target="_blank" href="https://se.linkedin.com/in/philewels">LinkedIn</a> |
           <a target="_blank" href="https://www.researchgate.net/profile/Philip_Ewels">ResearchGate</a>
         </p>
         <a target="_blank" href="http://www.scilifelab.se" class="scilife-footer">
@@ -144,66 +140,29 @@ $series_json = json_encode($series);
 
 	<!-- Scripts -->
 	<script src="js/jquery-1.11.3.min.js"></script>
-	<script src="//code.highcharts.com/highcharts.js"></script>
-	<script type="text/javascript">
-	$(function () {
-	  $('#multiqc_version_checks_plot').highcharts({
-	    chart: {
-	      type: 'area',
-        zoomType: 'x'
-	    },
-	    title: {
-	      text: 'MultiQC usage checks over time'
-	    },
-	    xAxis: {
-			  type: 'datetime',
-				dateTimeLabelFormats: {
-					millisecond: '%e %b %Y, %H:%M',
-					second: '%e %b %Y, %H:%M',
-					minute: '%e %b %Y, %H:%M',
-					hour: '%e %b %Y, %H:%M',
-					day: '%e %b %Y',
-					week: '%e %b %Y',
-					month: '%b \'%y',
-					year: '%Y'
-	      }
-	    },
-	    yAxis: {
-        min: 0,
-	      title: {
-	        text: 'Checks'
-	      },
-        reversedStacks: false
-	    },
-	    tooltip: {
-	      shared: true
-	    },
-      legend: { reversed: true },
-	    plotOptions: {
-	      area: {
-	        stacking: 'normal',
-	        lineColor: '#666666',
-	        lineWidth: 1,
-	        marker: {
-	          lineWidth: 1,
-	          lineColor: '#666666'
-	        }
-	      }
-	    },
-	    series: <?php echo $series_json; ?>
-	  });
-	});
-</script>
-<!-- Google Analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-68098153-1', 'auto');
+    ga('send', 'pageview');
 
-  ga('create', 'UA-68098153-1', 'auto');
-  ga('send', 'pageview');
+    // Plotly
+    Plotly.newPlot(
+      'versions_by_week',
+      <?php echo json_encode(array_values($versions_by_week)); ?>,
+      {
+        title: 'MultiQC: Version checks per week',
+        barmode: 'stack',
+        // bargap: 0,
+        paper_bgcolor: 'rgba(0,0,0,0)',
+        plot_bgcolor: 'rgba(0,0,0,0)',
+        yaxis: { gridcolor: '#dedede' }
+      }
+    );
 
-</script>
+  </script>
 	</body>
 </html>
--- public_html/version.php
+++ public_html/version.php
@@ -11,23 +11,25 @@ print($version);
 
 // Log version of tool that's asking
 if(isset($_GET['v'])){
-    $remote_version = str_replace('.dev0', 'dev', $_GET['v']);
+    $remote_version_pieces = explode(" ", $_GET['v']);
+    $remote_version = $remote_version_pieces[0];
+    $remote_version = str_replace('.dev0', '.dev', $remote_version);
     $dev = substr($remote_version, -3) == 'dev' ? 'dev' : '';
     $remote_version = preg_replace('/[^\d\.]/', '', $remote_version).$dev;
-    if($remote_version !== ''){
-        
-        // Connect to the database
-        $dbconfig = parse_ini_file("../config.ini");
-        $db = new mysqli('localhost', $dbconfig['user'], $dbconfig['password'], $dbconfig['db']);
-        if($db->connect_errno == 0){
-            
-            // Insert new record with querying version
-            $stmt = $db->prepare("INSERT INTO version_check (version) VALUES (?)");
-            $stmt->bind_param('s',$remote_version);
-            $stmt->execute();
-            $db->close();
-            
-        }
+
+    if ($remote_version == '') $remote_version = '< 0.5';
+
+    // Connect to the database
+    $dbconfig = parse_ini_file("../config.ini");
+    $db = new mysqli('localhost', $dbconfig['user'], $dbconfig['password'], $dbconfig['db']);
+    if($db->connect_errno == 0){
+
+        // Insert new record with querying version
+        $stmt = $db->prepare("INSERT INTO version_check (version) VALUES (?)");
+        $stmt->bind_param('s',$remote_version);
+        $stmt->execute();
+        $db->close();
+
     }
 }
 
