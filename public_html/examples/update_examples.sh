93c7093e63198339935f31d01b2799a030804c68
Phil Ewels <phil.ewels@scilifelab.se>
2018-03-15 16:36:58 +0100

    Updated examples to v1.5. Added script to automate this process.

---
+++ public_html/examples/update_examples.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+declare -a dirs=("bs-seq" "hi-c" "rna-seq" "wgs")
+
+for i in "${dirs[@]}"; do
+    cd $i
+    rm -rf multiqc_report.html multiqc_report.zip multiqc_data
+    multiqc .
+    zip -r multiqc_report.zip multiqc_report.html multiqc_data
+    cd ../
+done
+
+cd ngi-rna
+rm -rf *multiqc_report.html multiqc_report.zip *multiqc_data
+multiqc . --test-db ngi_db_data.json
+zip -r multiqc_report.zip *multiqc_report.html *multiqc_data
+cd ../
+
+open */*multiqc_report.html
