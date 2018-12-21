64503ae0e7748164aea092187e1b732f0c36c148
Phil Ewels <phil.ewels@scilifelab.se>
2018-12-21 18:59:00 +0100

    Disable NGI when preparing new reports

--- public_html/examples/update_examples.sh
+++ public_html/examples/update_examples.sh
@@ -5,7 +5,7 @@ declare -a dirs=("bs-seq" "hi-c" "rna-seq" "wgs")
 for i in "${dirs[@]}"; do
     cd $i
     rm -rf multiqc_report.html multiqc_report.zip multiqc_data
-    multiqc .
+    multiqc . --disable-ngi
     zip -r multiqc_report.zip multiqc_report.html multiqc_data
     cd ../
 done
