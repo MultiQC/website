8119cefb6d672e39e1e44448bac0a5d143f257b2
Phil Ewels <phil.ewels@scilifelab.se>
2019-10-07 14:24:44 +0200

    Fix installation type on homepage, better display

--- public_html/index.php
+++ public_html/index.php
@@ -165,7 +165,7 @@ if(count($md_parts) == 3){
               <div class="panel-body">
                 <pre id="pip"><code >pip install multiqc    <span class="comment"># Install</span>
 multiqc .              <span class="comment"># Run</span></code></pre>
-<pre id="conda" style="display:none;"><code>conda install -c bioconda -c conda-forge ultiqc
+<pre id="conda" style="display:none;"><code>conda install -c bioconda -c conda-forge multiqc
 multiqc .</code></pre>
 <pre id="manual" style="display:none;"><code>git clone https://github.com/ewels/MultiQC.git
 python setup.py install
