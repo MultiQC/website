4f8a78a98f6b4159ebe103a409d0056375e66ee0
Phil Ewels <phil.ewels@scilifelab.se>
2019-11-20 16:44:16 +0100

    Update example reports for MultiQC v1.8

--- public_html/examples/update_examples.sh
+++ public_html/examples/update_examples.sh
@@ -3,6 +3,9 @@
 declare -a dirs=("bs-seq" "hi-c" "rna-seq" "wgs")
 
 for i in "${dirs[@]}"; do
+    echo "--------------------------------------------------"
+    echo "Creating report for $i"
+    echo "--------------------------------------------------"
     cd $i
     rm -rf multiqc_report.html multiqc_report.zip multiqc_data
     multiqc . --disable-ngi -t default
@@ -10,10 +13,16 @@ for i in "${dirs[@]}"; do
     cd ../
 done
 
+echo "--------------------------------------------------"
+echo "Creating report for ngi-rna"
+echo "--------------------------------------------------"
 cd ngi-rna
 rm -rf *multiqc_report.html multiqc_report.zip *multiqc_data
 multiqc . --test-db ngi_db_data.json
 zip -r multiqc_report.zip *multiqc_report.html *multiqc_data
 cd ../
 
+echo "--------------------------------------------------"
+echo "Opening completed reports"
+echo "--------------------------------------------------"
 open */*multiqc_report.html
