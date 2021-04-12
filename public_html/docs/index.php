1207000209a782532b7ddf112aab70ba4916bd59
Phil Ewels <phil.ewels@scilifelab.se>
2021-04-12 11:48:13 +0200

    Escape hyphen in docs softlinks regex

--- public_html/docs/index.php
+++ public_html/docs/index.php
@@ -74,7 +74,7 @@ $content = preg_replace_callback(
   '~<h([1234])>([^<]*)</h([1234])>~Ui', // Ungreedy by default, case insensitive
   function ($matches) {
     global $hids;
-    $id_match = strtolower( @preg_replace('/[^\w-\.]/', '', str_replace(' ', '-', $matches[2])));
+    $id_match = strtolower( @preg_replace('/[^\w\-\.]/', '', str_replace(' ', '-', $matches[2])));
     $id_match = str_replace('---', '-', $id_match);
     $hid = $id_match;
     $i = 1;
