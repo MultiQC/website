665d1bcdb73c757c5bf5099f5aeaf0948c017957
Oleh Pratsko <olehpratsko@gmail.com>
2022-12-05 10:20:36 +0200

    feat: update footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {
+  GitHubIcon,
   Link,
+  LinkedInIcon,
   SeqeraIcon,
   SlackIcon,
   TwitterIcon,
@@ -10,36 +12,39 @@ import {
 const Footer = () => {
   return (
     <>
-      <footer className="bg-gray-900 text-white mt-auto relative">
+      <footer className="bg-gray-800 text-white mt-auto relative">
         <div className="container-lg">
           <div className="border-white border-t opacity-40" />
           <div className="flex justify-between items-center h-12">
-            <div>
-              <Link to="https://seqera.io/" noBorder className="typo-small">
-                &copy; 2022 Seqera Labs
-              </Link>
-            </div>
-            <div className="flex items-center md:justify-end">
+            <div className="flex items-center">
+              <div className="typo-small text-white mr-4">
+                Created by Phil Ewels:
+              </div>
               <div className="mr-4">
-                <Link to="https://twitter.com/hashtag/NextflowSummit?src=hashtag_click" className="hover:text-green-600" noBorder>
+                <Link to="https://github.com/ewels" className="hover:text-blue-600" noBorder>
+                  <GitHubIcon className="w-5 h-5" />
+                </Link>
+              </div>
+              <div className="mr-4">
+                <Link to="https://twitter.com/tallphil" className="hover:text-blue-600" noBorder>
                   <TwitterIcon className="w-5 h-5" />
                 </Link>
               </div>
               <div className="mr-4">
-                <Link to="https://www.youtube.com/c/nextflow" className="hover:text-green-600" noBorder>
-                  <YoutubeRectangleIcon className="w-5 h-5" />
+                <Link to="https://se.linkedin.com/in/philewels" className="hover:text-blue-600" noBorder>
+                  <LinkedInIcon className="w-5 h-5" />
                 </Link>
               </div>
               <div className="mr-4">
-                <Link to="https://www.nextflow.io/slack-invite.html" className="hover:text-green-600" noBorder>
-                  <SlackIcon className="w-5 h-5" />
+                <Link to="https://se.linkedin.com/in/philewels" className="typo-small hover:text-blue-600" noBorder>
+                  ResearchGate
                 </Link>
               </div>
-              <div>
-                <Link to="https://seqera.io" className="hover:text-green-600" noBorder>
-                  <SeqeraIcon className="w-4 h-4" />
-                </Link>
-              </div>
+            </div>
+            <div>
+              <Link to="https://seqera.io/" noBorder className="typo-small">
+                &copy; 2022 Seqera Labs
+              </Link>
             </div>
           </div>
         </div>
