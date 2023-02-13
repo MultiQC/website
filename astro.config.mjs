104493ca53d89baeeab4c750dd612edbf5137d7d
mashehu <mashehu3@gmail.com>
2023-02-13 15:49:31 +0100

    add quick install box as svelte component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,21 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
+import svelte from "@astrojs/svelte";
 export default defineConfig({
   site: "https://multiqc.info",
-  integrations: [tailwind({config: { applyBaseStyles: false }}), mdx(), prefetch(), sitemap()],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    mdx(),
+    prefetch(),
+    sitemap(),
+    svelte(),
+  ],
 });
