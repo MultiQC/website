cfd9b82b6e19c7d35a5f1f25c30b9f2fbdf73096
Phil Ewels <phil.ewels@seqera.io>
2023-03-02 11:17:36 +0100

    Run prettier

--- src/components/MarkdownWrapper.svelte
+++ src/components/MarkdownWrapper.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { currentHeading } from '@components/store.js';
+  import { onMount } from "svelte";
+  import { currentHeading } from "@components/store.js";
 
-    export let headings: { text: string; slug: string; depth: number;}[] = [];
+  export let headings: { text: string; slug: string; depth: number }[] = [];
 
-    // find current heading in viewport with IntersectionObserver
-    onMount(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                entries.forEach((entry) => {
-                    if (entry.isIntersecting) {
-                        currentHeading.set(entry.target.id);
-                    }
-                });
-            },
-            {
-                rootMargin: '0px 0px -92% 0px', // 92% of viewport height to offset navbar
-            }
-        );
-        headings.forEach((heading) => {
-            const element = document.querySelector('#' + heading.slug);
-            observer.observe(element);
+  // find current heading in viewport with IntersectionObserver
+  onMount(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            currentHeading.set(entry.target.id);
+          }
         });
+      },
+      {
+        rootMargin: "0px 0px -92% 0px", // 92% of viewport height to offset navbar
+      }
+    );
+    headings.forEach((heading) => {
+      const element = document.querySelector("#" + heading.slug);
+      observer.observe(element);
     });
+  });
 </script>
 
 <div class="markdown-content">
-    <slot />
+  <slot />
 </div>
