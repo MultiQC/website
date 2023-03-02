14b9e592182ba616b2286fc410b87ab05f37b6cb
mashehu <mashehu3@gmail.com>
2023-03-02 08:55:29 +0100

    smaller styling fixes

--- src/components/SidebarToc.svelte
+++ src/components/SidebarToc.svelte
@@ -26,7 +26,7 @@
                         class={'toc'}
                         class:active={heading.slug === $currentHeading}
                     >
-                        <a class= {headingPadding[heading.depth]+" text-xs block py-1 px-2 border-gray-200 dark:border-gray-700 hover:bg-blue-100 hover:!border-blue-600 dark:hover:text-gray-400 dark:hover:bg-blue-600/[0.2] dark:hover:!border-blue-600 border-l-4"} href={'#' + heading.slug}>
+                        <a class= {headingPadding[heading.depth]+" text-xs block py-1 px-2 border-gray-200 dark:border-gray-700 hover:bg-blue-100 hover:!border-blue-600 dark:hover:text-gray-600 dark:hover:bg-blue-600/[0.2] dark:hover:!border-blue-600 border-l-4"} href={'#' + heading.slug}>
                             {@html heading.text}
                         </a>
                     </li>
