8f929f0f8269c0a46fff2a483e701b8013284129
mashehu <mashehu3@gmail.com>
2023-03-03 12:57:00 +0100

    indicate theme change better in toggle

--- src/components/DarkModeToggle.svelte
+++ src/components/DarkModeToggle.svelte
@@ -17,13 +17,7 @@
   };
 </script>
 
-<button class="rounded-full bg-gray-800 px-2 py-1" on:click={() => toggleTheme()}>
-  <Icon
-    icon="ph:moon-bold"
-    class={"mr-1 inline h-4 w-4 " + (theme === "light" ? "text-gray-600" : "text-gray-300")}
-  />
-  <Icon
-    icon="ph:sun-bold"
-    class={"inline h-4 w-4  " + (theme !== "light" ? "text-gray-600" : "text-gray-300")}
-  />
+<button class="rounded-full bg-gray-200 px-2 py-1 dark:bg-gray-800" on:click={() => toggleTheme()}>
+  <Icon icon="ph:moon-bold" width="14" height="14" class={"mr-1 inline h-4 w-4 text-gray-300"} />
+  <Icon icon="ph:sun-bold" class={"inline h-4 w-4 text-gray-600"} />
 </button>
