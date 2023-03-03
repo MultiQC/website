98fa29e592eedc4abbb877b2c790fb852598d38a
Phil Ewels <phil.ewels@seqera.io>
2023-03-03 01:08:50 +0100

    Fix bugs only showing in build

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Global configuration variables
 
 export const SITE_TITLE = "MultiQC";
+export const SITE_AUTHOR = "Seqera Labs";
 export const SITE_DESCRIPTION =
   "Aggregate results from bioinformatics analyses across many samples into a single report";
