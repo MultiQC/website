14456bb5858dbc579a048b1be1eabc37f248df47
mashehu <mashehu3@gmail.com>
2023-03-03 13:41:35 +0100

    use astro icons in darkmode toggle

--- src/components/DarkModeToggle.svelte
+++ src/components/DarkModeToggle.svelte
@@ -18,6 +18,5 @@
 </script>
 
 <button class="" on:click={() => toggleTheme()}>
-  <Icon icon="ph:moon-bold" width="14" height="14" class={"mr-1 inline h-4 w-4 text-gray-300"} />
-  <Icon icon="ph:sun-bold" class={"inline h-4 w-4 text-gray-600"} />
+  <slot />
 </button>
