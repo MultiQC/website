d8900e63a0d3b9de061a4e080759e8fe5bc9dd9c
mashehu <mashehu3@gmail.com>
2023-03-08 14:59:29 +0100

    switch to astro assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-import { defineConfig } from "astro/config";
-import urls from "rehype-urls";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import yaml from "@rollup/plugin-yaml";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -27,15 +24,9 @@ export default defineConfig({
     plugins: [yaml()],
   },
   markdown: {
-    rehypePlugins: [
-      [
-        urls,
-        (url) => {
-          if (url.href.startsWith("../../images/")) {
-            return url.href.replace("../../images/", "/docs/images/");
-          }
-        },
-      ],
-    ],
+    rehypePlugins: [],
+  },
+  experimental: {
+    assets: true,
   },
 });
