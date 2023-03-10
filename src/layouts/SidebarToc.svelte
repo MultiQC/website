92caff4ce5bf3707cb92d9705f6f46eb012d9969
Phil Ewels <phil.ewels@seqera.io>
2023-03-10 12:20:54 +0100

    Exclude h1 from the ToC

--- src/layouts/SidebarToc.svelte
+++ src/layouts/SidebarToc.svelte
@@ -7,33 +7,24 @@
     depth: number;
   }[];
 
-  const show_min_heading_depth = 2;
-  const show_max_heading_depth = 3;
-  const min_heading_depth = Math.min(...headings.map((h) => h.depth)) - show_min_heading_depth + 3;
+  const min_heading_depth = Math.min(...headings.map((h) => h.depth));
 </script>
 
 <ul class="toc">
   {#each headings as heading (heading)}
-    <!--
-    {#if heading.depth >= show_min_heading_depth && heading.depth <= show_max_heading_depth}
-    -->
     <li class:active={heading.slug === $currentHeading}>
       <a
         class={"heading-level-" +
           heading.depth +
-          " block border-l-2 border-transparent py-1 px-3 text-xs " +
+          " block border-l-2 border-transparent py-1 px-2 text-xs " +
           " hover:bg-blue-100 hover:text-gray-900" +
           " dark:hover:bg-blue-600/[0.2] dark:hover:text-gray-300"}
         href={"#" + heading.slug}
         style="margin-left: {(heading.depth - min_heading_depth) * 0.5}rem; font-size: {90 -
           (heading.depth - min_heading_depth) * 10}%;"
-        depth={heading.depth}
       >
         {@html heading.text}
       </a>
     </li>
-    <!--
-    {/if}
-    -->
   {/each}
 </ul>
