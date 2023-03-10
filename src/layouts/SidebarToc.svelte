1a4306f44e3b63a483c658c573c7bbce72355c7c
Phil Ewels <phil.ewels@seqera.io>
2023-03-10 11:57:27 +0100

    Add styling hierarchy to table of contents

--- src/layouts/SidebarToc.svelte
+++ src/layouts/SidebarToc.svelte
@@ -7,19 +7,33 @@
     depth: number;
   }[];
 
-  const min_heading_depth = Math.min(...headings.map((h) => h.depth));
+  const show_min_heading_depth = 2;
+  const show_max_heading_depth = 3;
+  const min_heading_depth = Math.min(...headings.map((h) => h.depth)) - show_min_heading_depth + 3;
 </script>
 
 <ul class="toc">
   {#each headings as heading (heading)}
+    <!--
+    {#if heading.depth >= show_min_heading_depth && heading.depth <= show_max_heading_depth}
+    -->
     <li class:active={heading.slug === $currentHeading}>
       <a
-        class="block border-l-2 border-gray-200 py-1 pr-2 text-xs hover:!border-blue-600 hover:bg-blue-100 hover:text-gray-900 dark:border-gray-700 dark:hover:!border-blue-600 dark:hover:bg-blue-600/[0.2] dark:hover:text-gray-300"
+        class={"heading-level-" +
+          heading.depth +
+          " block border-l-2 border-transparent py-1 px-3 text-xs " +
+          " hover:bg-blue-100 hover:text-gray-900" +
+          " dark:hover:bg-blue-600/[0.2] dark:hover:text-gray-300"}
         href={"#" + heading.slug}
-        style="padding-left: {0.5 + (heading.depth - min_heading_depth) * 0.5}rem"
+        style="margin-left: {(heading.depth - min_heading_depth) * 0.5}rem; font-size: {90 -
+          (heading.depth - min_heading_depth) * 10}%;"
+        depth={heading.depth}
       >
         {@html heading.text}
       </a>
     </li>
+    <!--
+    {/if}
+    -->
   {/each}
 </ul>
