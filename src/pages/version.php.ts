cf8ec7665678da978d67108245172ebbc4db152f
Phil Ewels <phil.ewels@seqera.io>
2023-03-14 23:27:50 +0100

    First attempt at using prisma to connect to a database

--- src/pages/version.php.ts
+++ src/pages/version.php.ts
@@ -1,6 +1,31 @@
+import type { APIRoute } from "astro";
 import mqc_releases from "../multiqc_releases.json";
 
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+function log_call(version) {
+  console.log(version);
+
+  async function main() {
+    // ... you will write your Prisma Client queries here
+    console.log("HELLO");
+  }
+
+  main()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+}
+
 export const get: APIRoute = ({ params, request }) => {
+  console.log(params);
+  log_call(params);
   return new Response(mqc_releases.latest, {
     headers: { "content-type": "text/plain" },
     status: 200,
