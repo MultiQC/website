e89c30edb45693d3acca24a1161d437d160f69dc
Phil Ewels <phil.ewels@seqera.io>
2023-06-13 01:14:17 +0200

    Use min-light theme for codeblocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -90,7 +90,7 @@ export default defineConfig({
           keepBackground: true,
           theme: {
             dark: "one-dark-pro",
-            light: "github-light",
+            light: "min-light",
           },
         },
       ],
