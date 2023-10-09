59aa375c1c77e6f9b619e7a23340f36274bf6961
Phil Ewels <phil.ewels@seqera.io>
2023-10-09 11:30:48 +0200

    Revert "performance improvements"

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import urls from "rehype-urls";
 
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify/functions";
-import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
@@ -44,7 +43,6 @@ export default defineConfig({
     }),
     mdx(),
     prefetch(),
-    partytown(),
     sitemap(),
     svelte(),
   ],
