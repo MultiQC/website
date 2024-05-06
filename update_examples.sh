342d6f9251de5d71b4089819735d732e26b8e2b5
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2024-05-06 19:36:14 +0200

    Add notebook into multiqc_report.zip

--- update_examples.sh
+++ update_examples.sh
@@ -10,7 +10,7 @@ for i in "public/examples/${dirs[@]}"; do
     rm -rf multiqc_report.html multiqc_report.zip multiqc_data
     unzip -q data.zip
     multiqc . --disable-ngi -t default
-    zip -r multiqc_report.zip multiqc_report.html multiqc_data
+    zip -q -r multiqc_report.zip multiqc_report.html multiqc_data
     rm -r data/ multiqc_data/
     cd ../
 done
@@ -24,20 +24,23 @@ unzip -q data.zip
 multiqc . --test-db ngi_db_data.json
 # plugin changed the name of the report, don't want to break links
 mv test_ngi_project_pipeline_multiqc_report.html test_ngi_project_multiqc_report.html
-zip -r multiqc_report.zip *multiqc_report.html *multiqc_data
+zip -q -r multiqc_report.zip *multiqc_report.html *multiqc_data
 rm -r data/ test_ngi_project_multiqc_data/
+cd ../
+
+echo "--------------------------------------------------"
+echo "Creating Jupyter example"
+echo "--------------------------------------------------"
+cd jupyter
+rm -rf multiqc_report.zip multiqc_report.html multiqc_report_data
+unzip -q data.zip
+jupyter execute notebook.ipynb
+jupyter nbconvert --to html notebook.ipynb
+zip -q -r multiqc_report.zip notebook.ipynb multiqc_report.html multiqc_report_data
+rm -r data/ multiqc_report_data/
 cd ../
 
 echo "--------------------------------------------------"
 echo "Opening completed reports"
 echo "--------------------------------------------------"
-cd jupyter
-rm -rf *multiqc_report.html multiqc_report.zip *multiqc_report_data
-unzip -q data.zip
-jupyter execute notebook.ipynb
-jupyter nbconvert --to html notebook.ipynb
-zip -r multiqc_report.zip *notebook.html multiqc_report.html *multiqc_report_data
-rm -r data/ multiqc_report_data/
-cd ../
-
 open */*multiqc_report.html
