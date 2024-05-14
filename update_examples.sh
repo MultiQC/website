59b8ab1bcc93f272d5a887a0a3829349a9a2f9a7
Vlad Savelyev <vladislav.savelyev@populationgenomics.org.au>
2024-05-14 18:23:54 +0200

    Update examples for version 1.22 (#95)
    * Update examples for version 1.22
    
    * Update NGI
    
    * Clean up NGI folder
    
    * Update after fix
    
    * Update with removed heatmap labels
    
    * Remove macosx dir from data
    
    * Update the notebook example
    
    * Update the notebook example
    
    * Fix report_section_order: ngi-rnaseq -> ngi-rnaseq-module
    
    * Update after fixes

--- update_examples.sh
+++ update_examples.sh
@@ -14,18 +14,19 @@ for i in "public/examples/${dirs[@]}"; do
     rm -r data/ multiqc_data/
     cd ../
 done
+cd ../../
 
 echo "--------------------------------------------------"
 echo "Creating report for ngi-rna"
 echo "--------------------------------------------------"
-cd ngi-rna
+cd public/examples/ngi-rna
 rm -rf *multiqc_report.html multiqc_report.zip *multiqc_data
 unzip -q data.zip
 multiqc . --test-db ngi_db_data.json
 # plugin changed the name of the report, don't want to break links
-mv test_ngi_project_pipeline_multiqc_report.html test_ngi_project_multiqc_report.html
-zip -q -r multiqc_report.zip *multiqc_report.html *multiqc_data
-rm -r data/ test_ngi_project_multiqc_data/
+mv P1234-test_ngi_project_multiqc_report.html test_ngi_project_multiqc_report.html
+zip -q -r multiqc_report.zip *multiqc_report.html *multiqc_report_data
+rm -r data/ *multiqc_report_data
 cd ../
 
 echo "--------------------------------------------------"
@@ -40,16 +41,20 @@ wget https://github.com/MultiQC/example-notebook/raw/master/multiqc_example.ipyn
 sed -i '' 's/\%pip install/\# \%pip install/g' notebook.ipynb  # remove the pip install command
 sed -i '' 's/\%reset/\# \%reset/g' notebook.ipynb  # remove the kernel restart command
 # 2. GitHub doesn't render interactive plots, but the website does
-sed -i '' 's/, flat=True//g' notebook.ipynb  # remove the flat=True parameter
-sed -i '' '/flat=True/d' notebook.ipynb  # remove the explanation about flat=True
+sed -i '' 's/, flat=True//g' notebook.ipynb  # remove the flat=True parameters
+sed -i '' 's/(flat=True)/()/g' notebook.ipynb  # remove the flat=True parameters
+sed -i '' '/The rendered plot is a static image/d' notebook.ipynb  # remove the explanation about flat=True
 unzip -q data.zip
 jupyter execute notebook.ipynb --inplace  # Run the notebook
+sed -i '' 's/\# \%pip install/\%pip install/g' notebook.ipynb  # remove the pip install command
+sed -i '' 's/\# \%reset/\%reset/g' notebook.ipynb  # remove the kernel restart command
 jupyter nbconvert --to html notebook.ipynb  # Convert it to HTML
 zip -q -r multiqc_report.zip notebook.ipynb multiqc_report.html multiqc_report_data
-rm -r data/ multiqc_report_data/ notebook.ipynb
+rm -r data/ multiqc_report_data/ notebook.ipynb gc_content.*
 cd ../
 
 echo "--------------------------------------------------"
 echo "Opening completed reports"
 echo "--------------------------------------------------"
 open */*multiqc_report.html
+cd ../../
